17f61449db29183151784d767308128a6823593e
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-11-24 15:57:41 +0100

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>This is our app by: Oyinlade</h1>
+      <h1>This is our app by: Oyinlade</h1>
     </div>
   );
 }
