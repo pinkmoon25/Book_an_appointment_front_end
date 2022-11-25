4c3b0ad18c2a45c69f1430277f1a90d27c87d21b
adebowale <ademibowaleadegboye@gmail.com>
2022-11-25 16:15:29 +0100

    create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+// import { removeUser } from '../redux/user/user';
+// import { deleteUserApi } from '../redux/user/userAPI';
+import logo from '../codes_expert.png';
+
+const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const activeLink = ({ isActive }) => `nav-link${(isActive ? ' activated' : '')}`;
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+  }, [user]);
+
+  const closeMenu = () => {
+    setNavbarOpen(true);
+  };
+
+  const signOut = () => {
+    dispatch(removeUser());
+    localStorage.removeItem('user');
+  };
+
+  const deleteAccount = () => {
+    dispatch(deleteUserApi(user[0].user.user_id, user[0].token));
+    localStorage.removeItem('user');
+  };
+
+  return (
+    <nav>
+      <div className="btn-container">
+        { navbarOpen
+          ? <button className="menu-btn" onClick={handleToggle} type="button">&#9776;</button> 
+          : <button className="menu-btn" onClick={handleToggle} type="button">&#9747;</button>}
+      </div>
+      <div className={`logo-container ${navbarOpen ? ' closeMenu' : ''}`}>
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className={`nav-links ${navbarOpen ? ' closeMenu' : ''}`}>
+        <NavLink to="/" className="nav-link" onClick={() => closeMenu()}>Home</NavLink>
+        <NavLink to="/Reservations" className={activeLink} onClick={() => closeMenu()}>My Appointments</NavLink>
+        <NavLink to="/AddMentor" className={activeLink} onClick={() => closeMenu()}>Create Appointment</NavLink>
+        <NavLink to="/ReservationsForm" className={activeLink} onClick={() => closeMenu()}>Reserve Appointment</NavLink>
+        <NavLink to="/DeleteAppointments" className={activeLink} onClick={() => closeMenu()}>Delete Appointment</NavLink>
+      </div>
+
+      {user.length > 0
+        ? (
+          <div className={`sign-out ${navbarOpen ? ' closeMenu' : ''}`}>
+            <button className="sign-out-btn" type="button" onClick={signOut}>Sign Out</button>
+            <button className="sign-out-btn" type="button" onClick={deleteAccount}>Delete Account</button>
+          </div>
+        ) : ('')}
+    </nav>
+  );
+};
+
+export default Navbar;
