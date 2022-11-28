5581117b4a2451c3927c8a97b8aa153e55ae8a7e
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-28 20:52:36 +0100

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReservationForm from './pages/reservations/ReservationForm';
+import ReservationsPage from './pages/reservations/ReservationsPage';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
       <h1>This is our app by: Oyinlade</h1>
     </div>
   );
