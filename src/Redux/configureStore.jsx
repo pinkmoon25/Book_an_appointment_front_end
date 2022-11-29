078dfa395d5ccbd16d73254f6cd3417999509372
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-29 05:19:08 +0100

    Add functionality to reservation page

---
+++ src/Redux/configureStore.jsx
@@ -0,0 +1,2 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { coachesSlice } from '../pages/mentors/coachSlice';
--- src/pages/reservations/ReservationsPage.jsx
+++ src/pages/reservations/ReservationsPage.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+/* eslint-disable react/no-unescaped-entities */
+import React, { useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import { useDispatch, useSelector } from 'react-redux';
+import { addReservations } from '';
 
 function ReservationsPage() {
+  const dispatch = useDispatch();
+  const fetchReservationData = async () => {
+    const response = await fetch(
+      'http://localhost:3000/api/v1/reservations',
+    ).then((response) => response.json());
+    dispatch(addReservations(response));
+  };
+  useEffect(() => {
+    fetchReservationData();
+  }, []);
+  const reservations = useSelector((state) => state.reservation.reservations);
+
   return (
-    <div>ReservationsPage</div>
+    <div>
+      <h2>My Reservations</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Coach's Name</th>
+            <th>Date</th>
+            <th>City</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {reservations.map((reservation) => (
+            <tr key={reservation[0]}>
+              <td>{reservation[1]}</td>
+              <td>{reservation[2]}</td>
+              <td>{reservation[3]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
