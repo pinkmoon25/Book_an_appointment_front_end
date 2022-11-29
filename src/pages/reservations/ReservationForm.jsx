668f90b892a3216836cf2aa8bfb37e152df149a1
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-29 03:51:49 +0100

    Add form inputs

--- src/pages/reservations/ReservationForm.jsx
+++ src/pages/reservations/ReservationForm.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function ReservationForm() {
   return (
     <div>
       <h2>RESERVATION FORM</h2>
-      <p>Complete this form to make reservation</p>
+      <p>Kindly fill to make reservation(s)</p>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Coach</Form.Label>
+          <Form.Select
+            id="coachId"
+            name="coachId"
+            value={state.coachId}
+            onChange={handleChange}
+          >
+            <option value="default">Select a coach</option>
+            {coaches.map((coach) => (
+              <option key={coach.id} value={coach.id}>
+                {coach.name}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Reservation Date</Form.Label>
+          <Form.Control
+            id="reserve_date"
+            type="date"
+            name="reserve_date"
+            value={state.reserve_date}
+            placeholder="31/11/2022"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Reservation City</Form.Label>
+          <Form.Control
+            id="city"
+            type="text"
+            name="city"
+            value={state.city}
+            placeholder="Where?"
+            onChange={handleChange}
+          />
+          <Button type="submit">Submit Reservation</Button>
+        </Form.Group>
+      </Form>
     </div>
   );
 }
