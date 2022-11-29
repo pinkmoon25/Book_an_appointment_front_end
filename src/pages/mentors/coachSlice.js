4049e72bad8bd8185e596bd100fa161d2b079964
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-29 04:36:30 +0100

    Add  fetchCoaches function to coachesSlice

--- src/pages/mentors/coachSlice.js
+++ src/pages/mentors/coachSlice.js
@@ -1,1 +1,15 @@
+/* eslint-disable import/prefer-default-export */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchCoaches = createAsyncThunk('coaches/fetchingCoaches', async (id) => {
+  const response = await fetch('http://localhost:3000/api/v1/coaches');
+  const coaches = await response.json();
+  return coaches.data;
+});
+
+export const deleteCoach = createAsyncThunk('coaches/deleteCoach', async (id) => {
+  const response = await fetch(`http://localhost:3000/api/v1/coaches/${id}`,
+    { method: 'delete' });
+const coaches = await response.json();
+return coaches;
+});
