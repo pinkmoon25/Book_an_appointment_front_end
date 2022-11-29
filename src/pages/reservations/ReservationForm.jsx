d1bc099672ba6cdd5a2e2d11268c69363d8379d4
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-29 04:01:56 +0100

    Add dispach action to reservation form

--- src/pages/reservations/ReservationForm.jsx
+++ src/pages/reservations/ReservationForm.jsx
@@ -1,9 +1,12 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function ReservationForm() {
-  
+  const dispatch = useDispatch();
+  const fetchCoaches = () => {
+     
+   }
 
   const [message, setMessage] = useState('');
 
