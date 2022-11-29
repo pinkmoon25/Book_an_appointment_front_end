768fe1868b47f0270613ff5441fcb1eebc477dc1
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-29 03:21:31 +0100

    Add reservation form page

--- src/pages/reservations/ReservationForm.jsx
+++ src/pages/reservations/ReservationForm.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function ReservationForm() {
   return (
-    <div>ReservationForm</div>
+    <div>
+      <h2>RESERVATION FORM</h2>
+      <p>Complete this form to make reservation</p>
+    </div>
   );
 }
 
