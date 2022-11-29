d23f013405cbb8aa8cdbd1ef9c6ebf6f0184df12
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-29 05:49:08 +0100

    Add reservation reducer to store

--- src/Redux/configureStore.jsx
+++ src/Redux/configureStore.jsx
@@ -1,2 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { coachesSlice } from '../pages/mentors/coachSlice';
+import reservationReducer from '../pages/reservations/reservationSlice';
+
+const store = configureStore({
+  reducer: {
+    coaches: coachesSlice,
+    reservation: reservationReducer,
+  },
+});
+export default store;
