4fa373a3e336b98465c968e13603458bc5b997dc
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-30 06:18:41 +0100

    Add schedule appointmemts

--- src/Redux/appointmentApi.js
+++ src/Redux/appointmentApi.js
@@ -11,3 +11,23 @@ export const getAllAppointments = async (baseURL, token) => {
   const result = await response.json();
   return result; // [{ id: '...', name: '...', date: '...', duration: '...', total: '...' }]
 };
+
+export const scheduleAppointment = async (baseURL, token, date, duration, doctorId) => {
+  const response = await fetch(`${baseURL}/appointments`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        appointment: {
+          date,
+          duration,
+          doctor_id: doctorId,
+        },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${token}`,
+      },
+    });
+  const result = await response.json();
+  return result; // { message: '...', status: '...' }
+};
