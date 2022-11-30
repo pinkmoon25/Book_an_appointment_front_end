b274ade26ee0bc6904adf75c19e347b756d26fa6
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-30 06:14:13 +0100

    Add test data

---
+++ src/data/coaches.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: 'Tushar',
+    stack: 'React',
+    image: 'https://via.placeholder.com/150/771796',
+  },
+  {
+    id: 'Oyin',
+    name: 'Vue',
+    image: 'https://via.placeholder.com/150/771796',
+  },
+  {
+    id: 'Adebowale',
+    name: 'Python',
+    image: 'https://via.placeholder.com/150/771796',
+  },
+  {
+    id: 'Gbenga',
+    name: 'Gatsby',
+    image: 'https://via.placeholder.com/150/771796',
+  },
+];
