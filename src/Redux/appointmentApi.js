532533b470957c7e52beca91d5fc93673e7084ce
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-30 06:19:58 +0100

    Add cancel Appointment method

--- src/Redux/appointmentApi.js
+++ src/Redux/appointmentApi.js
@@ -31,3 +31,16 @@ export const scheduleAppointment = async (baseURL, token, date, duration, doctor
   const result = await response.json();
   return result; // { message: '...', status: '...' }
 };
+
+export const cancelAppointment = async (baseURL, token, id) => {
+  const response = await fetch(`${baseURL}/appointments/${id}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${token}`,
+      },
+    });
+  const result = await response.json();
+  return result; // { message: '...', status: '...' }
+};
