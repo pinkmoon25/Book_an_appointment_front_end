76e1ed398780f0867ab3484a163c5343d24d8680
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-30 06:26:11 +0100

    import storage from LocalStorage

--- src/Redux/appointmentsSlice.js
+++ src/Redux/appointmentsSlice.js
@@ -1,2 +1,3 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { cancelAppointment, getAllAppointments, scheduleAppointment } from './appointmentsApi';
+import storage from '../../app/localStorage';
