8f5c27939466ad27acb29c682b4cd989f590718a
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-30 06:27:23 +0100

    Add base URL and Token

--- src/Redux/appointmentsSlice.js
+++ src/Redux/appointmentsSlice.js
@@ -1,3 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { cancelAppointment, getAllAppointments, scheduleAppointment } from './appointmentsApi';
 import storage from '../../app/localStorage';
+const baseURL = process.env.REACT_APP_BASE_URL;
+const token = storage.get('token');
+
