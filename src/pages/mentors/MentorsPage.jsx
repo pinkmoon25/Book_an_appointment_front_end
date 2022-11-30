7c9cd687c6699cb47eb86bd03e7bfc129f1aa3bf
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-11-30 16:04:04 +0100

    Add carousel

--- src/pages/mentors/MentorsPage.jsx
+++ src/pages/mentors/MentorsPage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css'
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import styles from '../../css/mentors.module.css';
 import { getMentors } from '../../Redux/mentors/mentors';
@@ -23,7 +25,7 @@ const MentorsPage = () => {
           <p className="text-secondary">{mentor.bio}</p>
           <div>
             {mentor.skills.map((skill) =>(
-              <><span>{skill.name}</span> <img src={skill.icon} alt={skill.name}/></>
+              <><span>{skill.name}</span></>
             ))}
           </div>
         </div>
@@ -38,7 +40,55 @@ const MentorsPage = () => {
           <h3>LIST OF Mentors</h3>
           <p className="text-secondary">Please select a mentor to view details</p>
         </div>
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className=""
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite={false}
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: 3,
+              partialVisibilityGutter: 40,
+            },
+            mobile: {
+              breakpoint: {
+                max: 767,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 768,
+              },
+              items: 2,
+              partialVisibilityGutter: 30,
+            },
+          }}
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
         {mentorsList}
+        </Carousel>
       </div>
     </>
   );
