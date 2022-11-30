4ef8ad46abdd815388832c5d1132a16f6147cd56
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-11-30 06:28:36 +0100

    Add getALLappointmentsAsync

--- src/Redux/appointmentsSlice.js
+++ src/Redux/appointmentsSlice.js
@@ -4,3 +4,10 @@ import storage from '../../app/localStorage';
 const baseURL = process.env.REACT_APP_BASE_URL;
 const token = storage.get('token');
 
+export const getAllAppointmentsAsync = createAsyncThunk(
+  'appointments/getAllAppointments',
+  async () => {
+    const response = await getAllAppointments(baseURL, token);
+    return response;
+  },
+);
