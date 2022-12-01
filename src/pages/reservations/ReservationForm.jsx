e32f96198bb7f7b33081d450f01fb5095b84b4da
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-12-01 16:00:14 +0100

    Fix linters

--- src/pages/reservations/ReservationForm.jsx
+++ src/pages/reservations/ReservationForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
