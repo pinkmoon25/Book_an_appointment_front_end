9f6bebe38e08905d613db247422bdbe3d914fea8
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-12-01 15:58:15 +0100

    Fix linter

--- src/pages/reservations/ReservationForm.jsx
+++ src/pages/reservations/ReservationForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { getMentors } from '../../Redux/mentors/mentors';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { getMentors } from '../../Redux/mentors/mentors';
 
 const ReservationForm = () => {
   const [date, setDate] = useState('');
@@ -10,34 +10,31 @@ const ReservationForm = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   useEffect(() => {
-    dispatch(getMentors)
+    dispatch(getMentors);
   },
-    []
-  )
+  []);
   let mentor = mentors.find((item) => parseInt(item.id, 10) === parseInt(id, 10));
-  if (!mentor) { localStorage.setItem('item', JSON.stringify(mentor)) }
+  if (!mentor) { localStorage.setItem('item', JSON.stringify(mentor)); }
   mentor = JSON.parse(localStorage.getItem('item'));
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
   return (
     <div>
       <form onSubmit={(e) => handleSubmit(e)}>
         <h1>Reservation</h1>
-        <input name='date' type='date' value={date} onChange={(e) => setDate(e.target.value)} />
+        <input name="date" type="date" value={date} onChange={(e) => setDate(e.target.value)} />
         <select name="skills" id="skills">
-          <option value='react'>react</option>
+          <option value="react">react</option>
         </select>
-        <input type='submit' value='Book' />
+        <input type="submit" value="Book" />
       </form>
       {(mentor.skills?.map((s) => {
-        console.log(s.name)
-        console.log(s)
+        console.log(s.name);
+        console.log(s);
       }))}
 
     </div>
-  )
-}
-export default ReservationForm
-
-    
+  );
+};
+export default ReservationForm;
