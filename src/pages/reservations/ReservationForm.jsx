4679f3c099d98c53d2e141dcfdca3248ad01186d
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-12-01 15:45:42 +0100

    Add reservation form component

--- src/pages/reservations/ReservationForm.jsx
+++ src/pages/reservations/ReservationForm.jsx
@@ -2,3 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { getMentors } from '../../Redux/mentors/mentors';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+
+const ReservationForm = () => {
+  const [date, setDate] = useState('');
