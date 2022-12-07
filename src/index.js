4b050f1077560bda715485a5546d6be7d1614a55
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2022-12-07 12:38:53 +0100

    Add the provider and persistor

--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@ import reportWebVitals from './reportWebVitals';
 import store from './Redux/configureStore';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+   <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
 
 reportWebVitals();
+
+
